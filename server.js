47fd6ff3eef390c8a69d128d8092b1455b0fd592
gr8geek <pratyush9000@gmail.com>
2020-08-29 15:36:13 +0530

    Change 2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http')
 var server = http.createServer((req,res)=>{
     res.writeHead(200,{'Content-Type':'text/html'});
-    res.end('<h1>Hie</h1>')
+    res.end('<h1>Hello beautiful world</h1>')
 }).listen(8080)
 //console.log("listening Bro..",")
